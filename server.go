85bc7f1a266145b06bf393eed1c0b722f451408a
david <david@dcarew.com>
2021-07-04 07:18:36 -0700

    Add go module

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "log"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+type Record struct {
+    Date int `json:"date"`
+    Words int `json:"words"`
+    Desc string `json:"desc"`
+}
+
+
+var schema = "CREATE TABLE IF NOT EXISTS wordcount(date INTEGER NOT NULL, words INTEGER NOT NULL, desc STRING)"
+
+func initDatabase(path string) *sql.DB {
+    database, err := sql.Open("sqlite3", path)
+    if err != nil {
+        log.Fatal("Could not open database.")
+    }
+    stmt, _ := database.Prepare(schema)
+    stmt.Exec()
+
+    return database
+}
+
+func main() {
+    initDatabase("test.db")
+
+}
+
+
+
